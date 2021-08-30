e0a02fe11db10be5450ff494333a559235f80c7d
Nicolo Maioli <nicolomaioli@gmail.com>
2021-08-30 13:57:35 +0100

    Set up state for login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-function App() {
+import React, { useState, useEffect } from 'react'
+import Amplify, { Auth } from 'aws-amplify'
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
+import awsconfig from './aws-exports'
+
+Amplify.configure(awsconfig)
+
+function App () {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [user, setUser] = useState({})
+
+  const isLoggedIn = async () => {
+    try {
+      const user = await Auth.currentAuthenticatedUser()
+      setUser(user)
+      setLoggedIn(true)
+    } catch (err) {
+      setLoggedIn(false)
+    }
+  }
+
+  useEffect(() => {
+    isLoggedIn()
+  }, [])
+
   return (
     <div>
-      <p>Hello World!</p>
+      <h1>Hello World! { loggedIn ? user.getUsername() : ''}</h1>
+      <AmplifySignOut />
     </div>
-  );
+  )
 }
 
-export default App;
+export default withAuthenticator(App)
