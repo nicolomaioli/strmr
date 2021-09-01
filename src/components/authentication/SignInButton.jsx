ede042362704941b9edd970d24f00ea94312df8a
Nicolo Maioli <nicolomaioli@gmail.com>
2021-09-01 15:07:01 +0100

    Remove user context

--- src/components/authentication/SignInButton.jsx
+++ src/components/authentication/SignInButton.jsx
@@ -1,23 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import Button from "@material-ui/core/Button";
 import { NavLink } from "react-router-dom";
 
-import { useUser } from "../../contexts/UserCtx";
-import { getUser } from "../../lib/auth";
-
 export default function SignInButton() {
-  const { setUser } = useUser();
-
-  useEffect(() => {
-    const handleUser = async () => {
-      const user = await getUser();
-      setUser(user);
-    };
-
-    handleUser();
-  }, [setUser]);
-
   return (
     <Button
       component={NavLink}
