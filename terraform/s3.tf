1cf14eb196082119b599a2b25f5694b7d02b2b24
Nicolo Maioli <nicolomaioli@gmail.com>
2021-09-02 13:03:02 +0100

    Update CORS rules
    Cfr https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/s3-example-photo-album.html

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,6 +2,20 @@ resource "aws_s3_bucket" "videos" {
   bucket = "${var.application}-videos-${terraform.workspace}"
   acl    = "private"
 
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = [
+      "HEAD",
+      "GET",
+      "PUT",
+      "POST",
+      "DELETE",
+    ]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+
   tags = {
     Application = var.application
     Environment = terraform.workspace
