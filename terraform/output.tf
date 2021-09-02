dd615ac707125903b34fc0e4d25b934b43262bab
Nicolo Maioli <nicolomaioli@gmail.com>
2021-09-02 12:52:25 +0100

    Create Identity Pool

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,6 +6,10 @@ output "user_pool_client_id" {
   value = aws_cognito_user_pool_client.this.id
 }
 
+output "identity_pool_id" {
+  value = aws_cognito_identity_pool.this.id
+}
+
 output "s3_videos_arn" {
   value = aws_s3_bucket.videos.arn
 }
