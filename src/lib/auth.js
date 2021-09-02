cfc771f58f3afab170e8bbdeba5991ea7a926ba2
Nicolo Maioli <nicolomaioli@gmail.com>
2021-09-02 11:50:29 +0100

    Add method to get current session

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -8,10 +8,14 @@ export async function signOut() {
   return Auth.signOut();
 }
 
+export async function signIn(username, password) {
+  return Auth.signIn(username, password);
+}
+
 export async function getUser() {
   return Auth.currentAuthenticatedUser();
 }
 
-export async function signIn(username, password) {
-  return Auth.signIn(username, password);
+export async function getCurrentSession() {
+  return Auth.currentSession();
 }
