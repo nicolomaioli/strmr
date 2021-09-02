140fb81ccea47138d38728db6ca8c6ef9b29acb4
Nicolo Maioli <nicolomaioli@gmail.com>
2021-09-02 13:11:23 +0100

    Update CORS policy for Amplify Storage
    Cfr: https://docs.amplify.aws/lib/storage/getting-started/q/platform/js/#amazon-s3-bucket-cors-policy-setup

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,6 +4,8 @@ resource "aws_s3_bucket" "videos" {
 
   cors_rule {
     allowed_headers = ["*"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
     allowed_methods = [
       "HEAD",
       "GET",
@@ -11,9 +13,12 @@ resource "aws_s3_bucket" "videos" {
       "POST",
       "DELETE",
     ]
-    allowed_origins = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
+    expose_headers = [
+      "x-amz-server-side-encryption",
+      "x-amz-request-id",
+      "x-amz-id-2",
+      "ETag"
+    ]
   }
 
   tags = {
