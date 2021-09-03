94cc5312371d104c7ed4d851fa973c6d9ea6e619
Nicolo Maioli <nicolomaioli@gmail.com>
2021-09-03 16:47:18 +0100

    Add success feedback

--- src/components/video/Upload.jsx
+++ src/components/video/Upload.jsx
@@ -8,17 +8,34 @@ import Grid from "@material-ui/core/Grid";
 import Input from "@material-ui/core/Input";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Paper from "@material-ui/core/Paper";
+import Snackbar from "@material-ui/core/Snackbar";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
+import MuiAlert from "@material-ui/lab/Alert";
 
 import { putObject } from "../../lib/storage";
 
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 export default function Upload() {
+  const [success, setSuccess] = useState(false);
   const [error, setError] = useState(null);
   const [progress, setProgress] = useState(0);
 
   const progressCallback = (p) => {
+    const currentProgress = Math.trunc((p.loaded * 100) / p.total);
     setProgress(Math.trunc((p.loaded * 100) / p.total));
+
+    if (currentProgress === 100) {
+      setSuccess(true);
+      setProgress(0);
+    }
+  };
+
+  const handleClose = () => {
+    setSuccess(false);
   };
 
   const handleChange = async (e) => {
@@ -34,66 +51,73 @@ export default function Upload() {
   };
 
   return (
-    <Box mt={4}>
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid item xs={12} sm={9} md={6}>
-          <Box m={2}>
-            <Paper variant="outlined" square>
-              <Box m={2}>
-                <Typography component="h1" variant="h5">
-                  Upload a video
-                </Typography>
-                {error && (
-                  <Typography color="error">{error.message}</Typography>
-                )}
-                <FormControl component="form" fullWidth>
-                  <Box mt={3}>
-                    <TextField
-                      label="Tile"
-                      name="title"
-                      placeholder="Title"
-                      required
-                      fullWidth
-                    />
-                  </Box>
-                  <Box mt={3}>
-                    <Input
-                      accept="image/*"
-                      style={{ display: "none" }}
-                      id="raised-button-file"
-                      onChange={(e) => handleChange(e)}
-                      required
-                      type="file"
-                    />
-                    <FormLabel htmlFor="raised-button-file">
-                      <Button variant="contained" component="span">
-                        Select File
+    <React.Fragment>
+      <Box mt={4}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} sm={9} md={6}>
+            <Box m={2}>
+              <Paper variant="outlined" square>
+                <Box m={2}>
+                  <Typography component="h1" variant="h5">
+                    Upload a video
+                  </Typography>
+                  {error && (
+                    <Typography color="error">{error.message}</Typography>
+                  )}
+                  <FormControl component="form" fullWidth>
+                    <Box mt={3}>
+                      <TextField
+                        label="Tile"
+                        name="title"
+                        placeholder="Title"
+                        required
+                        fullWidth
+                      />
+                    </Box>
+                    <Box mt={3}>
+                      <Input
+                        accept="image/*"
+                        style={{ display: "none" }}
+                        id="raised-button-file"
+                        onChange={(e) => handleChange(e)}
+                        required
+                        type="file"
+                      />
+                      <FormLabel htmlFor="raised-button-file">
+                        <Button variant="contained" component="span">
+                          Select File
+                        </Button>
+                      </FormLabel>
+                    </Box>
+                    <Box mt={3}>
+                      <Button
+                        type="submit"
+                        color="primary"
+                        variant="contained"
+                        // onClick={(e) => handleSubmit(e)}
+                        fullWidth
+                      >
+                        Upload
                       </Button>
-                    </FormLabel>
-                  </Box>
-                  <Box mt={3}>
-                    <Button
-                      type="submit"
-                      color="primary"
-                      variant="contained"
-                      // onClick={(e) => handleSubmit(e)}
-                      fullWidth
-                    >
-                      Upload
-                    </Button>
-                  </Box>
-                </FormControl>
-              </Box>
-            </Paper>
-            <LinearProgress variant="determinate" value={progress} />
-          </Box>
+                    </Box>
+                  </FormControl>
+                </Box>
+              </Paper>
+              <LinearProgress variant="determinate" value={progress} />
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+      <Snackbar open={success} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          File successfully uploaded
+        </Alert>
+      </Snackbar>
+    </React.Fragment>
   );
 }
