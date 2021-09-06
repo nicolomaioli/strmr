bf0bd64e2635487e503e384bfd37a157998c03d1
Nicolo Maioli <nicolomaioli@gmail.com>
2021-09-06 14:03:11 +0100

    Fix attribute names

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -2,16 +2,16 @@ resource "aws_dynamodb_table" "this" {
   name = "${var.application}-videos-${terraform.workspace}"
 
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "UserId"
+  hash_key     = "Username"
   range_key    = "CreatedAt"
 
   attribute {
-    name = "UserId"
+    name = "Username"
     type = "S"
   }
 
   attribute {
-    name = "VideoId"
+    name = "VideoID"
     type = "S"
   }
 
@@ -21,19 +21,19 @@ resource "aws_dynamodb_table" "this" {
   }
 
   attribute {
-    name = "Type"
+    name = "FileType"
     type = "S"
   }
 
   global_secondary_index {
     name            = "VideoIndex"
-    hash_key        = "VideoId"
+    hash_key        = "VideoID"
     projection_type = "ALL"
   }
 
   global_secondary_index {
     name            = "TypeCreatedAtIndex"
-    hash_key        = "Type"
+    hash_key        = "FileType"
     range_key       = "CreatedAt"
     projection_type = "ALL"
   }
