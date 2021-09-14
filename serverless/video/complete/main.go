a29b070966cbe76d8971b596f7cf4ba77ad553ec
Nicolo Maioli <nicolomaioli@gmail.com>
2021-09-14 16:17:59 +0100

    Update record on completed job

--- serverless/video/complete/main.go
+++ serverless/video/complete/main.go
@@ -7,17 +7,51 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/nicolomaioli/strmr-infra/serverless/video/common"
 )
 
+func updateRecord(ctx context.Context, cfg aws.Config, d *common.MediaConvertEventDetail) error {
+	client := dynamodb.NewFromConfig(cfg)
+
+	in := &dynamodb.UpdateItemInput{
+		TableName: &common.VideoTableName,
+		Key: map[string]types.AttributeValue{
+			"ID": &types.AttributeValueMemberS{
+				Value: d.UserMetadata["id"],
+			},
+		},
+		UpdateExpression: aws.String("set Status = :status"),
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":status": &types.AttributeValueMemberS{
+				Value: "COMPLETED",
+			},
+		},
+	}
+
+	_, err := client.UpdateItem(ctx, in)
+	return err
+}
+
 func Handler(ctx context.Context, e events.CloudWatchEvent) {
-	var d common.MediaConvertEventDetail
-	err := json.Unmarshal(e.Detail, &d)
+	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("Completed video: %s", d.UserMetadata["id"])
+	var d common.MediaConvertEventDetail
+	err = json.Unmarshal(e.Detail, &d)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = updateRecord(ctx, cfg, &d)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
