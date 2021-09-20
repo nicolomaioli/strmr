c934fa39c73c39998b02b518e87e94d21a967f65
Nicolo Maioli <nicolomaioli@gmail.com>
2021-09-20 14:13:49 +0100

    Add API Stage and Mappings
    This allows to connect the domain to the API Gateway

--- terraform/api.tf
+++ terraform/api.tf
@@ -30,3 +30,17 @@ resource "aws_apigatewayv2_domain_name" "this" {
 
   tags = local.tags
 }
+
+resource "aws_apigatewayv2_stage" "this" {
+  api_id      = aws_apigatewayv2_api.this.id
+  name        = terraform.workspace
+  auto_deploy = false
+
+  tags = local.tags
+}
+
+resource "aws_apigatewayv2_api_mapping" "this" {
+  api_id      = aws_apigatewayv2_api.this.id
+  domain_name = aws_apigatewayv2_domain_name.this.id
+  stage       = aws_apigatewayv2_stage.this.id
+}
